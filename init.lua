2a594c9ff67549d921f97edfefd4e71e0124d035
Samppa Alatalo <samppa.alatalo@siemens.com>
2025-09-15 15:30:48 +0300

    Update conform.nvim config to leave visual mode after range format

--- init.lua
+++ init.lua
@@ -683,10 +683,17 @@ require('lazy').setup({
       {
         '<leader>f',
         function()
-          require('conform').format { async = true }
+          require('conform').format({ async = true }, function(err)
+            if not err then
+              local mode = vim.api.nvim_get_mode().mode
+              if vim.startswith(string.lower(mode), 'v') then
+                vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes('<Esc>', true, false, true), 'n', true)
+              end
+            end
+          end)
         end,
         mode = '',
-        desc = '[F]ormat buffer',
+        desc = '[F]ormat code',
       },
     },
     ---@module 'conform'
