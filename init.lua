022619423cfddad5e3c1325ef31d6f0949daa103
Samppa Alatalo <18483942+Suamppa@users.noreply.github.com>
2025-10-14 22:09:28 +0300

    Add Harpoon

--- init.lua
+++ init.lua
@@ -394,6 +394,47 @@ require('lazy').setup({
     end,
   },
 
+  {
+    'ThePrimeagen/harpoon',
+    branch = 'harpoon2',
+    dependencies = { 'nvim-lua/plenary.nvim', 'nvim-telescope/telescope.nvim' },
+    opts = {},
+    config = function()
+      local harpoon = require('harpoon')
+      harpoon:setup({})
+
+      -- basic telescope configuration
+      local conf = require('telescope.config').values
+      local function toggle_telescope(harpoon_files)
+        local file_paths = {}
+        for _, item in ipairs(harpoon_files.items) do
+          table.insert(file_paths, item.value)
+        end
+
+        require('telescope.pickers').new({}, {
+          prompt_title = 'Harpoon',
+          finder = require('telescope.finders').new_table({
+            results = file_paths,
+          }),
+          previewer = conf.file_previewer({}),
+          sorter = conf.generic_sorter({}),
+        }):find()
+      end
+
+      vim.keymap.set('n', '<C-e>', function() toggle_telescope(harpoon:list()) end, { desc = 'Open harpoon window' })
+      vim.keymap.set('n', '<leader>a', function() harpoon:list():add() end, { desc = '[A]dd file to harpoon' })
+
+      vim.keymap.set("n", "<C-a>", function() harpoon:list():select(1) end, { desc = 'Go to harpoon file 1' })
+      vim.keymap.set("n", "<C-s>", function() harpoon:list():select(2) end, { desc = 'Go to harpoon file 2' })
+      vim.keymap.set("n", "<C-d>", function() harpoon:list():select(3) end, { desc = 'Go to harpoon file 3' })
+      vim.keymap.set("n", "<C-f>", function() harpoon:list():select(4) end, { desc = 'Go to harpoon file 4' })
+
+      -- Toggle previous & next buffers stored within Harpoon list
+      vim.keymap.set("n", "<C-J>", function() harpoon:list():prev() end, { desc = 'Go to previous harpoon file (down)' })
+      vim.keymap.set("n", "<C-K>", function() harpoon:list():next() end, { desc = 'Go to next harpoon file (up)' })
+    end,
+  },
+
   -- LSP Plugins
   {
     -- `lazydev` configures Lua LSP for your Neovim config, runtime and plugins
