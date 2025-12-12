2c24ec242b27430881d7fb2dbd9f6c8f1b07fb01
Samppa Alatalo <samppa.alatalo@siemens.com>
2025-12-12 12:45:26 +0200

    Remove deprecated rzls.nvim and update

--- init.lua
+++ init.lua
@@ -689,7 +689,6 @@ require('lazy').setup({
         },
         html = {},
         roslyn = {},
-        rzls = {},
       }
 
       -- Ensure the servers and tools above are installed
@@ -1063,42 +1062,9 @@ require('lazy').setup({
 
   {
     'seblyng/roslyn.nvim',
-    dependencies = {
-      {
-        'tris203/rzls.nvim',
-        config = true,
-      },
-    },
     ---@module 'roslyn.config'
     ---@type RoslynNvimConfig
     opts = {},
-    config = function()
-      local rzls_path = vim.fn.expand '$MASON/packages/rzls/libexec'
-      local cmd = {
-        'roslyn',
-        '--stdio',
-        '--logLevel=Information',
-        '--extensionLogDirectory=' .. vim.fs.dirname(vim.lsp.get_log_path()),
-        '--razorSourceGenerator=' .. vim.fs.joinpath(rzls_path, 'Microsoft.CodeAnalysis.Razor.Compiler.dll'),
-        '--razorDesignTimePath=' .. vim.fs.joinpath(rzls_path, 'Targets', 'Microsoft.NET.Sdk.Razor.DesignTime.targets'),
-        '--extension',
-        vim.fs.joinpath(rzls_path, 'RazorExtension', 'Microsoft.VisualStudioCode.RazorExtension.dll'),
-      }
-
-      vim.lsp.config('roslyn', {
-        cmd = cmd,
-        handlers = require 'rzls.roslyn_handlers',
-      })
-      vim.lsp.enable 'roslyn'
-    end,
-    init = function()
-      vim.filetype.add {
-        extension = {
-          razor = 'razor',
-          cshtml = 'razor',
-        },
-      }
-    end,
   },
 
   {
